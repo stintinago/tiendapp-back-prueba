076375b536f134632d1eae2950ebd52b14fe8bee
Santiago Tintinago <tinti261999@gmail.com>
2022-07-11 21:33:32 -0500

    fixed edit

--- routes/api.php
+++ routes/api.php
@@ -17,11 +17,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/products',[\App\Http\Controllers\ProductController::class, 'all']);
 Route::get('/product/{id}',[\App\Http\Controllers\ProductController::class, 'get']);
 Route::post('/products',[\App\Http\Controllers\ProductController::class, 'create']);
-Route::put('/products/{id}',[\App\Http\Controllers\ProductController::class, 'update']);
+Route::post('/productsUpdate/{id}',[\App\Http\Controllers\ProductController::class, 'update']);
 Route::delete('/products/{id}',[\App\Http\Controllers\ProductController::class, 'delete']);
 
 Route::get('/brands',[\App\Http\Controllers\BrandController::class, 'all']);
 Route::get('/brand/{id}',[\App\Http\Controllers\BrandController::class, 'get']);
 Route::post('/brands',[\App\Http\Controllers\BrandController::class, 'create']);
-Route::put('/brands/{id}',[\App\Http\Controllers\BrandController::class, 'update']);
+Route::post('/brandsUpdate/{id}',[\App\Http\Controllers\BrandController::class, 'update']);
 Route::delete('/brands/{id}',[\App\Http\Controllers\BrandController::class, 'delete']);
