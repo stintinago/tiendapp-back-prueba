d8c90ff0f29c6da46489f4c695167efecef2f2ca
Santiago Tintinago <tinti261999@gmail.com>
2022-07-25 13:50:23 -0500

    Brand request

--- app/Http/Controllers/BrandController.php
+++ app/Http/Controllers/BrandController.php
@@ -2,33 +2,46 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\BrandPost;
 use Illuminate\Http\Request;
 use App\Models\Brand;
+
 class BrandController extends Controller
 {
-    public function all () {
+    public function all()
+    {
         return Brand::all();
     }
 
-    public function get ($id){
+    public function get($id)
+    {
         return Brand::find($id);
     }
 
-    public function create (Request $request){
+    public function create(BrandPost $request)
+    {
+        $brand = Brand::create($request->validated());
+
+        return response()->json($brand, status:201);
+    }
+
+    /*public function create (Request $request){
         return Brand::create([
             'name' =>$request['name'],
             'reference' =>$request['reference']
         ]);
-    }
+    }*/
 
-    public function update (Request $request, $id){
+    public function update(Request $request, $id)
+    {
         return Brand::find($id)->update([
-            'name' =>$request['name'],
-            'reference' =>$request['reference']
+            'name' => $request['name'],
+            'reference' => $request['reference']
         ]);
     }
 
-    public function delete ($id) {
+    public function delete($id)
+    {
         return Brand::find($id)->delete();
     }
 }
